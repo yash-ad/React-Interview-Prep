f87b0245b84978b09376c78426b2f8dc1bf3da94
Yash Dandnaik <yashrajdandnaik.com>
2024-03-30 12:04:25 +0530

    Background changer  React project

--- InterviewQuestionCounter/src/App.jsx
+++ InterviewQuestionCounter/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(15)
 
+  console.log(count);
 
   const incrementValue = ()=>{
       //Now here the basic interview question?
@@ -13,8 +14,8 @@ function App() {
       // setCount(count + 1);
       // setCount(count + 1);
 
-      //Here in the second approach what we are doing, we are using callback function within setState function and it accepts.
-    // so here batches wont work it takes a callback function and add  a previous value,the useState hook updates the state and add a value on it.
+      //Here in the second approach what we are doing is, we are using callback function within setState function and it accepts.
+    // so here batches wont work it takes a callback function and add a previous value,the useState hook updates the state and add a value on it.
 setCount(prevCount => prevCount + 1);
 setCount(prevCount => prevCount + 1);
 setCount(prevCount => prevCount + 1);
