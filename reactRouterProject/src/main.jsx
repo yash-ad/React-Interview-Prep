d3e67cdc4ceb938c868ee5b8ee00263faec4d256
Yash Dandnaik <yashrajdandnaik.com>
2024-04-03 20:51:28 +0530

    React routing comopleted till createBrowserRouter

--- reactRouterProject/src/main.jsx
+++ reactRouterProject/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {Route, RouterProvider, createBrowserRouter ,createRoutesFromElements} from 'react-router-dom'
 import './index.css'
+import Layout from './Layout'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
+const router = createBrowserRouter(
+createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+<Route path='' element={<Home/>}/>
+<Route path='about' element={<About/>}/>
+<Route path='contact' element={<Contact/>}/>
+  </Route>
+)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router}/>
   </React.StrictMode>,
 )
