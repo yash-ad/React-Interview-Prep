04da836b76fbb6d08f9fabe17b8cfbc863a457dc
Yash Dandnaik <yashrajdandnaik.com>
2024-04-03 17:51:49 +0530

    React router project  for reactRouterDom

---
+++ reactRouterProject/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center bg-green-700">
+      React Router Project
+    </h1>
+    </>
+  )
+}
+
+export default App
