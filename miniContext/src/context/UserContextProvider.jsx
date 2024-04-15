dfc666e00686f9c20c377662cdaab5427ba252f0
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-04-15 13:37:17 +0530

    Context API part one project begin

---
+++ miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const UserContextProvider = ()=>{
+return(
+
+
+
+)   
+};
+
+
+export  default UserContextProvider
---
+++ miniContext/src/context/Usercontext.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//This is a method
+//Every context is a provider
+const UserContext = React.createContext();
+
+
+export default UserContext;
