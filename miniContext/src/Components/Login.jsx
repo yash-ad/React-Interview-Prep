6f916f98dc2c22e6b176568bf6c316f1f85bdadf
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-04-16 13:01:28 +0530

    code updated with practice

--- miniContext/src/Components/Login.jsx
+++ miniContext/src/Components/Login.jsx
@@ -1,31 +1,35 @@
-import {useState, useContext} from 'react'
-import UserContext from '../context/UserContext'
+import { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
 
-function Login() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+const Login = ()=>{
+  const[username,setUsername] = useState('')
+  const[password,setPassword] = useState('')
 
-    const {setUser} = useContext(UserContext)
+  const {setUser} = useContext(UserContext)
+  const handleSubmit = (event)=>{
+event.preventDefault();
+setUser({username,password})
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setUser({username, password})
-    }
-  return (
-    <div>
-        <h2>Login</h2>
-        <input type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value) }
-        placeholder='username'  />
-        {" "}
-        <input type='password' 
-        value={password}
-        onChange={(e) => setPassword(e.target.value) }
-        placeholder='password'  />
-        <button onClick={handleSubmit}>Submit</button>
-    </div>
+  return(
+<div>
+  <h2>Login</h2>
+  <input
+    type="text"
+    value={username}
+placeholder="username"
+onChange={(event)=> setUsername(event.target.value)}
+  />
+  {"    "}
+    <input
+    type="password"
+    value={password}
+placeholder="password"
+onChange={(event)=> setPassword(event.target.value)}
+  />
+  <button onClick={handleSubmit}>Submit</button>
+</div>
   )
 }
 
-export default Login
+export default Login;
