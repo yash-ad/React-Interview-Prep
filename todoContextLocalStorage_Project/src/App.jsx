3f588939f89fae946c38ee96df0c6a6e6d73dbb7
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-04-17 21:28:25 +0530

    Added methods into the App.jsx for functionaty

--- todoContextLocalStorage_Project/src/App.jsx
+++ todoContextLocalStorage_Project/src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react'
+import {TodoProvider} from './contexts'
+
 export default function App() {
+
+  const[todos,setTodos] = useState([]);
+
+  const addTodo = (todo)=>{
+setTodos((prev)=> [{id:Date.now(),...todo},...prev])
+  }
+  
   return (
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
     <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -11,5 +22,6 @@ export default function App() {
         </div>
     </div>
 </div>
+</TodoProvider>
   )
 }
