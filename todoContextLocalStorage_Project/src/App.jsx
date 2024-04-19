0f304f37abca663711353af12971b99a04576026
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-04-19 20:19:00 +0530

    Understood the depth of TodoForm how its works and its functionalities

--- todoContextLocalStorage_Project/src/App.jsx
+++ todoContextLocalStorage_Project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import {TodoProvider} from './contexts/Index'
+import TodoForm from './components/TodoForm';
 
 function App() {
 
@@ -7,20 +8,23 @@ function App() {
   //State Creation of Todo:-
   const[todos,setTodos]= useState([])
 
+
+
   ///Lets create functionalities for Adding,Updating,Deleting and  for checking the items:-
   
   //1.Add TODO functionality:-
-  const addTodo = (todo)=>{
-    //And how to modify a state variable By using setState function and the function comes with the second parameter of an array.
+  //And how to modify a state variable By using setState function and the function comes with the second parameter of an array.
 //Here we have accessed a callback function into the setState function because we want a previous values whenever we add a new todos, if we directly add a new todo into the setState function it just creates a new todo and updates the UI with a new values , we are unable to access a previous todos.
- //here we are also creating dynamic values using `Date.now()` to generate specific values
+//here we are also creating dynamic values using `Date.now()` to generate specific values.
+  const addTodo = (todo)=>{
 setTodos((prev)=> [{id:Date.now(),...todo},...prev])
   };
 
 //2.update TODO functionality:-
 //We know that  'todos' is an array and array must be loop to find an individual value, and here we are using map.
 //Every Individual todo will find , and every Individual todo is an object and every object has an Id,
-//here the task is every individual todo it is an object and it has a specific Id, so we want that that individual id is matching to the updatTodo id .
+//here the task is every individual todo it is an object and it has a specific Id, so we want that that individual id is matching to the updateTodo id .
+//If it is the update the todo that means here is an edit with the todo msg or otherwise remain prevTodo as it is.
 const updateTodo = (id,todo)=>{
 setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id ? todo : prevTodo))
 };
@@ -28,17 +32,20 @@ setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id ? todo : prevTodo))
 
 //3.Delete TODO functionality:-
 //Here we want to delete the specific Id using filter method , because we know that the Filter Method checks the condition with a boolean value either it true or false
-//Here to acheive the goal is to find the Id and it is not matching  to the given id which has passed from the deleteTodos method
+//Here to acheive the goal is to find the Id of a prevTodo and it is not matching  to the given id which has passed from the deleteTodos method
 const deleteTodo = (id)=>{
 setTodos((prev)=> prev.filter((prevTodo)=> prevTodo.id !==  id))
 };
 
 
 //4.toggle Checkbox TODO functionality:-
-//Here in this functionality we want to acheive is to check the item for the task is completed, the goal is to acheive the matching and Id using map to loop on an existing values or for previous values , if there would be match it edits into the UI and add a new todo
+//Here in this functionality we want to acheive is to check the item for the task is completed, the goal is to acheive the matching an Id using map to loop on an existing values or for previous values.
 //with using not operator to opposites the value if there it is false and the overwrite convert them into true and if there is True then convert them into the false.Thats it!
 const toggleComplete = (id)=>{
-setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id)? {...prevTodo,completed:!prevTodo.completed} :prevTodo)
+setTodos((prev)=> 
+prev.map((prevTodo)=> 
+prevTodo.id === id ? {...prevTodo,
+  completed:!prevTodo.completed} :prevTodo))
 }
 
 
@@ -50,13 +57,13 @@ setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id)? {...prevTodo,comple
 //And here is an empty dependency array with init that means it will render only just once initially.
 
 //localStorage.getItem("key");
-useEffect(()=>{
-const todos = JSON.parse(localStorage.getItem("todos"));
 //Lets check the condition if the "todos" exist or not
 //True && True -> True
 //True && false -> false
 //false && True -> false
 //false && false -> false
+useEffect(()=>{
+const todos = JSON.parse(localStorage.getItem("todos"));
 if(todos && todos.length < 0 ){
   setTodos(todos)
 }
@@ -71,14 +78,16 @@ if(todos && todos.length < 0 ){
 useEffect(()=>{
 localStorage.setItem("todos",JSON.stringify(todos))
 },[todos])
+
   return (
-    //Values are extracted  and destrcutured from an object , which came from a context
+    //Values are extracted  and destrcutured from an object , which came from a context.
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
 <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
---
+++ todoContextLocalStorage_Project/src/components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import  { useState } from 'react'
+import {useTodo} from '../contexts/Index'
+
+function TodoForm() {
+    
+
+//First of all lets define the state to an individual todo which we have created into the App.jsx
+const [todo,setTodo] = useState('');
+
+//Extract the values from useTodo () hook which comes from context.
+const{addTodo} = useTodo();
+
+const add = (event)=>{
+event.preventDefault();
+if(!todo) return
+addTodo({todo,completed:false})
+setTodo("")
+}
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            //Wiring
+            value={todo}
+onChange={(event)=> setTodo(event.target.value)}
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
